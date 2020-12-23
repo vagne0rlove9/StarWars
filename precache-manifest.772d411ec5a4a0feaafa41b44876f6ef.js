f52857a71d5fafb7ebaebef65ed18274e2035ea8
vagne0rlove9 <kirik-pos@mail.ru>
2020-12-23 21:33:11 +0300

    Updates

---
+++ precache-manifest.772d411ec5a4a0feaafa41b44876f6ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6837eebc6580141849d14fb0a5d5546",
+    "url": "/StarWars/index.html"
+  },
+  {
+    "revision": "9398399339dd1b8cf765",
+    "url": "/StarWars/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "5b093dd70f16413e1f82",
+    "url": "/StarWars/static/css/main.a793ee79.chunk.css"
+  },
+  {
+    "revision": "9398399339dd1b8cf765",
+    "url": "/StarWars/static/js/2.83307e8a.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/StarWars/static/js/2.83307e8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b093dd70f16413e1f82",
+    "url": "/StarWars/static/js/main.dc6e38d6.chunk.js"
+  },
+  {
+    "revision": "948a4f60426f7b7fbc58",
+    "url": "/StarWars/static/js/runtime-main.5756cce3.js"
+  }
+]);
