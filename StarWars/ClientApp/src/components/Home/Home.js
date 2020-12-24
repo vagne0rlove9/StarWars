e18d09a503b1588a94393eda24a19b43129e77da
vagne0rlove9 <kirik-pos@mail.ru>
2020-12-24 23:35:07 +0300

    fix img

--- StarWars/ClientApp/src/components/Home/Home.js
+++ StarWars/ClientApp/src/components/Home/Home.js
@@ -14,7 +14,7 @@ class Home extends Component {
 
         dices.push({
             color: event.target.id,
-            face: Math.floor(Math.random() * 6) + 1 
+            face: Math.floor(Math.random() * 6) + 1
         })
 
         this.setState({ dicesAttack: dices })
@@ -43,98 +43,99 @@ class Home extends Component {
 
     render() {
         return (
-	<>
-            <img className="img-bg" alt="" src="images/bg2.jpg" />
-            <div className="main">
-                <div className="div-dices">
-                    Кубы атаки
+            <>
+                <img className="img-bg" alt="" src="images/bg2.jpg" />
+                <div className="main">
+                    <div className="div-dices">
+                        Кубы атаки
                     <div className="div-attack-dice">
-                        <img
-                            className="img-dice"
-                            src="dices/yellow0.png"
-                            alt=""
-                            id="yellow"
-                            onClick={this.handleClikcDiceAttack}
-                        />
-                        <img
-                            className="img-dice"
-                            src="dices/blue0.png"
-                            alt=""
-                            id="blue"
-                            onClick={this.handleClikcDiceAttack}
-                        />
-                        <img
-                            className="img-dice"
-                            src="dices/green0.png"
-                            alt=""
-                            id="green"
-                            onClick={this.handleClikcDiceAttack}
-                        />
-                        <img
-                            className="img-dice"
-                            src="dices/red0.png"
-                            alt=""
-                            id="red"
-                            onClick={this.handleClikcDiceAttack}
-                        />
-                    </div>
-
-                    <div className="div-attack-dice">
-                        Пул кубов атаки
-                        <div className="div-attack-dice">
-                            {
-                                this.state.dicesAttack.length !== 0 ?
-                                    this.state.dicesAttack.map((dice, index) => {
-                                        return <Dice color={dice.color} key={index} face={dice.face} />
-                                    })
-                                    : null
-                            }
+                            <img
+                                className="img-dice"
+                                src="dices/yellow0.png"
+                                alt=""
+                                id="yellow"
+                                onClick={this.handleClikcDiceAttack}
+                            />
+                            <img
+                                className="img-dice"
+                                src="dices/blue0.png"
+                                alt=""
+                                id="blue"
+                                onClick={this.handleClikcDiceAttack}
+                            />
+                            <img
+                                className="img-dice"
+                                src="dices/green0.png"
+                                alt=""
+                                id="green"
+                                onClick={this.handleClikcDiceAttack}
+                            />
+                            <img
+                                className="img-dice"
+                                src="dices/red0.png"
+                                alt=""
+                                id="red"
+                                onClick={this.handleClikcDiceAttack}
+                            />
                         </div>
-                    </div>
-                    <div className="div-button">
-                        <button onClick={this.handleClikcDeleteDicesAttack} className="button-delete">Удалить кубы</button>
-                    </div>
 
-                </div>
-
-                <div className="div-dices">
-                    Кубы защиты
-                    <div className="div-attack-dice">
-                        <img
-                            className="img-dice"
-                            src="dices/white0.png"
-                            alt=""
-                            id="white"
-                            onClick={this.handleClikcDiceDefence}
-                        />
-                        <img
-                            className="img-dice"
-                            src="dices/black0.png"
-                            alt=""
-                            id="black"
-                            onClick={this.handleClikcDiceDefence}
-                        />
-                    </div>
-
-                    <div className="div-attack-dice">
-                        Пул кубов защиты
                         <div className="div-attack-dice">
-                            {
-                                this.state.dicesDefence.length !== 0 ?
-                                    this.state.dicesDefence.map((dice, index) => {
-                                        return <Dice color={dice.color} key={index} face={dice.face} />
-                                    })
-                                    : null
-                            }
+                            Пул кубов атаки
+                        <div className="div-attack-dice">
+                                {
+                                    this.state.dicesAttack.length !== 0 ?
+                                        this.state.dicesAttack.map((dice, index) => {
+                                            return <Dice color={dice.color} key={index} face={dice.face} />
+                                        })
+                                        : null
+                                }
+                            </div>
                         </div>
-                    </div>
-                    <div className="div-button">
-                        <button onClick={this.handleClikcDeleteDicesDefence} className="button-delete">Удалить кубы</button>
+                        <div className="div-button">
+                            <button onClick={this.handleClikcDeleteDicesAttack} className="button-delete">Удалить кубы</button>
+                        </div>
+
                     </div>
 
+                    <div className="div-dices">
+                        Кубы защиты
+                    <div className="div-attack-dice">
+                            <img
+                                className="img-dice"
+                                src="dices/white0.png"
+                                alt=""
+                                id="white"
+                                onClick={this.handleClikcDiceDefence}
+                            />
+                            <img
+                                className="img-dice"
+                                src="dices/black0.png"
+                                alt=""
+                                id="black"
+                                onClick={this.handleClikcDiceDefence}
+                            />
+                        </div>
+
+                        <div className="div-attack-dice">
+                            Пул кубов защиты
+                        <div className="div-attack-dice">
+                                {
+                                    this.state.dicesDefence.length !== 0 ?
+                                        this.state.dicesDefence.map((dice, index) => {
+                                            return <Dice color={dice.color} key={index} face={dice.face} />
+                                        })
+                                        : null
+                                }
+                            </div>
+                        </div>
+                        <div className="div-button">
+                            <button onClick={this.handleClikcDeleteDicesDefence} className="button-delete">Удалить кубы</button>
+                        </div>
+
+                    </div>
                 </div>
-            </div>
-	</>
+               
+            </>
         );
     }
 }
